405d6e005cec90a389f7739e1f64118ebc503e57
Bryce Eadie <bryce.eadie@gmail.com>
2018-12-16 02:15:45 +0000

    Automatically backed up by Learn

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -4,5 +4,6 @@ module PostsHelper
       select_tag "post[author_id]", options_from_collection_for_select(Author.all, :id, :name)
     else
       hidden_field_tag "post[author_id]", post.author_id
+    end
   end 
 end
